189d3342a4746bfae40847517b5e5791bbed5f58
Ap.Muthu <apmuthu@usa.net>
2018-01-21 00:59:35 +0530

    @BraathWaate: rep_bank_daily_sweep module added https://github.com/braathwaate/FA24extensions/commit/71663f8e7a0f07e85bafc0c682fb949c4ae84aa6

---
+++ Extensions/rep_bank_daily_sweep/reporting/reports_custom.php
@@ -0,0 +1,25 @@
+<?php
+// include_once($path_to_root . "/includes/ui/ui_lists.inc");
+
+global $reports, $dim;
+
+function bank_sweep($param, $type)
+{
+    if ($type == 'BANK_SWEEP') {
+        return bank_accounts_list($param, 9999, false, "-- No Sweep --");
+    }
+}
+
+$reports->register_controls("bank_sweep");
+
+$reports->addReport(RC_BANKING, "_bank_daily_sweep",_('&Bank Daily Summary w/Sweep'),
+	array(  _('Bank Accounts') => 'BANK_ACCOUNTS',
+			_('Start Date') => 'DATEBEGINM',
+			_('End Date') => 'DATEENDM',
+			_('Zero values') => 'YES_NO',
+			_('Sweep To Bank Account') => 'BANK_SWEEP',
+			_('Comments') => 'TEXTBOX',
+			_('Orientation') => 'ORIENTATION',
+			_('Destination') => 'DESTINATION'));
+
+?>
