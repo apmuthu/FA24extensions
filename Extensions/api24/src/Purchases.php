ee849a230bf3d24459caedcab3ea764eb247641d
Ap.Muthu <apmuthu@usa.net>
2018-06-24 18:11:00 +0530

    Replaced Tab with 4 spaces in API specific files

--- Extensions/api24/src/Purchases.php
+++ Extensions/api24/src/Purchases.php
@@ -4,62 +4,62 @@ namespace FAAPI;
 include_once (FA_ROOT . "/purchasing/includes/db/supp_trans_db.inc");
 
 class Purchases {
-	// Get Items
-	public function get($rest) {
-		$req = $rest->request();
+    // Get Items
+    public function get($rest) {
+        $req = $rest->request();
 
-		$page = $req->get("page");
+        $page = $req->get("page");
 
-		$sql = get_sql_for_supplier_inquiry(ALL_TEXT, '1/1/0000', '1/1/9999');
-		$result = db_query($sql, 'oops');
-		
-		$body = array();
+        $sql = get_sql_for_supplier_inquiry(ALL_TEXT, '1/1/0000', '1/1/9999');
+        $result = db_query($sql, 'oops');
+        
+        $body = array();
 
-		while ($row = db_fetch_assoc($result)) {
-			$body[] = $row;
-		}
-		
-		api_success_response(json_encode($body));
-	}
-	
-	public function getByType($rest, $trans_type) {
-		$req = $rest->request();
+        while ($row = db_fetch_assoc($result)) {
+            $body[] = $row;
+        }
+        
+        api_success_response(json_encode($body));
+    }
+    
+    public function getByType($rest, $trans_type) {
+        $req = $rest->request();
 
-		$page = $req->get("page");
+        $page = $req->get("page");
 
-		$sql = get_sql_for_supplier_inquiry(ALL_TEXT, '0000-01-01', '9999-12-31');
-		$result = db_query($sql, 'oops');
-		
-// 		if ($page == null) {
-// 			sales_all($trans_type);
-// 		} else {
-// 			// If page = 1 the value will be 0, if page = 2 the value will be 1, ...
-// 			$from = -- $page * RESULTS_PER_PAGE;
-// 			sales_all($trans_type, $from);
-// 		}
-	}
+        $sql = get_sql_for_supplier_inquiry(ALL_TEXT, '0000-01-01', '9999-12-31');
+        $result = db_query($sql, 'oops');
+        
+//         if ($page == null) {
+//             sales_all($trans_type);
+//         } else {
+//             // If page = 1 the value will be 0, if page = 2 the value will be 1, ...
+//             $from = -- $page * RESULTS_PER_PAGE;
+//             sales_all($trans_type, $from);
+//         }
+    }
 
-	// Get Specific Item by Id
-	public function getById($rest, $trans_no, $trans_type) {
-		include_once (API_ROOT . "/sales.inc");
-		sales_get($trans_no, $trans_type);
-	}
+    // Get Specific Item by Id
+    public function getById($rest, $trans_no, $trans_type) {
+        include_once (API_ROOT . "/sales.inc");
+        sales_get($trans_no, $trans_type);
+    }
 
-	// Add Item
-	public function post($rest) {
-		include_once (API_ROOT . "/sales.inc");
-		sales_add();
-	}
+    // Add Item
+    public function post($rest) {
+        include_once (API_ROOT . "/sales.inc");
+        sales_add();
+    }
 
-	// Edit Specific Item
-	public function put($rest, $trans_no, $trans_type) {
-		include_once (API_ROOT . "/sales.inc");
-		sales_edit($trans_no, $trans_type);
-	}
+    // Edit Specific Item
+    public function put($rest, $trans_no, $trans_type) {
+        include_once (API_ROOT . "/sales.inc");
+        sales_edit($trans_no, $trans_type);
+    }
 
-	// Delete Specific Item
-	public function delete($rest, $branch_id, $uuid) {
-		include_once (API_ROOT . "/sales.inc");
-		sales_cancel($branch_id, $uuid);
-	}
+    // Delete Specific Item
+    public function delete($rest, $branch_id, $uuid) {
+        include_once (API_ROOT . "/sales.inc");
+        sales_cancel($branch_id, $uuid);
+    }
 }
--- Extensions/api24/util.php
+++ Extensions/api24/util.php
@@ -6,62 +6,62 @@ Free software under GNU GPL
 ***********************************************/
 
 function api_login() {
-	$app = \Slim\Slim::getInstance('SASYS');
-	$app->hook('slim.before', function () use ($app) {
-		$req = $app->request();
-		$company = $req->headers('X-COMPANY');
-		$user = $req->headers('X-USER');
-		$password = $req->headers('X-PASSWORD');
+    $app = \Slim\Slim::getInstance('SASYS');
+    $app->hook('slim.before', function () use ($app) {
+        $req = $app->request();
+        $company = $req->headers('X-COMPANY');
+        $user = $req->headers('X-USER');
+        $password = $req->headers('X-PASSWORD');
 
-		// TESTING
-		/*$company = 0;
-		$user = 'admin';
-		$password = '123';*/
+        // TESTING
+        /*$company = 0;
+        $user = 'admin';
+        $password = '123';*/
 
-		$succeed = $_SESSION["wa_current_user"]->login($company,
-					$user, $password);
-		if(!$succeed) {
-			$app->halt(403, 'Bad Login For Company: ' . $company . ' With User: ' . $user);
-		}
-	}, 1);
+        $succeed = $_SESSION["wa_current_user"]->login($company,
+                    $user, $password);
+        if(!$succeed) {
+            $app->halt(403, 'Bad Login For Company: ' . $company . ' With User: ' . $user);
+        }
+    }, 1);
 }
 
 function api_response($code, $body) {
-	$app = \Slim\Slim::getInstance('SASYS');
-	$app->response()->status($code);
-	$app->response()->body($body);
+    $app = \Slim\Slim::getInstance('SASYS');
+    $app->response()->status($code);
+    $app->response()->body($body);
 }
 
 function api_success_response($body) {
-	$app = \Slim\Slim::getInstance('SASYS');
-	$app->response()->status(200);
-	$app->response()->body($body);
-	//$app->response()->['Content-Type'] = $content_type;
+    $app = \Slim\Slim::getInstance('SASYS');
+    $app->response()->status(200);
+    $app->response()->body($body);
+    //$app->response()->['Content-Type'] = $content_type;
 }
 
 function api_create_response($body) {
-	$app = \Slim\Slim::getInstance('SASYS');
-	$app->response()->status(201);
-	$app->response()->body($body);
-	//$app->response()->['Content-Type'] = $content_type;
+    $app = \Slim\Slim::getInstance('SASYS');
+    $app->response()->status(201);
+    $app->response()->body($body);
+    //$app->response()->['Content-Type'] = $content_type;
 }
 
 function api_error($code, $msg) {
-	$app = \Slim\Slim::getInstance('SASYS');
-	$app->halt($code, json_encode(array('success' => 0, 'msg' => $msg)));
+    $app = \Slim\Slim::getInstance('SASYS');
+    $app->halt($code, json_encode(array('success' => 0, 'msg' => $msg)));
 
 }
 
 function api_ensureAssociativeArray($a) {
-	if (!$a) {
-		$a = array();
-	}
-	foreach ($a as $key => $value) {
-		if (is_int($key)) {
-			unset($a[$key]);
-		}
-	}
-	return $a;
+    if (!$a) {
+        $a = array();
+    }
+    foreach ($a as $key => $value) {
+        if (is_int($key)) {
+            unset($a[$key]);
+        }
+    }
+    return $a;
 }
 
 ?>
