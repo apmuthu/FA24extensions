190e974c6ed9b02288022f14455339e70733d9e7
trafficpest <trafficpestsolutions@gmail.com>
2024-02-16 01:41:51 -0700

    adding route deliveries plugin

---
+++ Extensions/rep_route_deliveries/route_config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+  'field_label' => 'Geocoding',
+  'swap_cords' => false,
+  'home_point' => '-118.5653429,34.3771202',
+  'osrm_url' => 'http://router.project-osrm.org/trip/v1/car/',
+  'km' => false,
+);
+
